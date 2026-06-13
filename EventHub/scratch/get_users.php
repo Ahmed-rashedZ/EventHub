cbc33dff25e4a5590c92e33428a14f4859bac949
2211844085 <mrxly12345@gmail.com>
2026-06-13 07:15:03 +0200

    ملفات زايدة في النظام

--- EventHub/scratch/get_users.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$profiles = DB::table('profiles')->get();
-echo json_encode($profiles, JSON_PRETTY_PRINT);
